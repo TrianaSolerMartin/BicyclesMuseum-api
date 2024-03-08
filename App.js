af29880d4830f16abcb04c1c4996bba9eee94c9f
albamartinmz <albamartinsonido@gmail.com>
2024-03-08 09:39:42 +0100

    get method working

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import conection_db from "./database/connection_db.js";
+import connection_db from "./database/connection_db.js";
+import BicycleModel from "./models/BicycleModel.js";
+import express from 'express';
+import { PORT } from "./config.js";
+import BicycleRouter from './routers/BicycleRouter.js'
+
+
+const app = express();
+
+app.use('/api', BicycleRouter)
 
 try {
-    await conection_db.authenticate();
-    console.log('❤  ❤  ❤ Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  // Autenticar la conexión
+  await connection_db.authenticate();
+  console.log('❤️❤️❤️ Connection has been established successfully.');
+
+  // Sincronizar el modelo BicycleModel con la base de datos
+  await BicycleModel.sync();
+  console.log('✅ BicycleModel has been synchronized with the database.');
+
+} catch (error) {
+  console.error('❌ Unable to connect to the database:', error);
+}
+
+app.listen(PORT, () => {
+  console.log(`Server up in http://localhost:${PORT}/api`);
+});
