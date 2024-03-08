28d75b8ee54ff777a5b5f370c1272193ba2c8f54
TrianaSolerMartin <triana287@gmail.com>
2024-03-08 09:57:53 +0100

    publish develop without method

--- routers/BicycleRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllBicycles } from '../controllers/BicycleController.js';
-
-const router = express.Router();
-
-router.get('/', getAllBicycles)
-
-export default router;
