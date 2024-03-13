94726e515b22ab92ed305124e883df9043e7f151
TrianaSolerMartin <triana287@gmail.com>
2024-03-13 14:14:30 +0100

    first test working

--- validators/bicycleValidator.js
+++ validators/bicycleValidator.js
@@ -2,6 +2,7 @@ import { check } from 'express-validator';
 import { validateResult } from '../helpers/validateHelper.js';
 
 export const validateCreate = [
+
     check('model')
         .exists()
         .notEmpty(),
