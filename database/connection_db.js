a22935f5779552fccda39a8fc77321864f6f45a5
TrianaSolerMartin <triana287@gmail.com>
2024-03-13 14:45:17 +0100

    3 test and new db_de_name working

--- database/connection_db.js
+++ database/connection_db.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
-import {DB_NAME, DB_USER, DB_PASSWORD} from '../config.js';
+import {DB_DEV_NAME, DB_USER, DB_PASSWORD, DB_TEST_NAME, NODE_ENV} from '../config.js';
 
+const DB_NAME =  NODE_ENV === 'test'? DB_TEST_NAME:DB_DEV_NAME
 // Crear conexión a la base de datos utilizando las variables de entorno
 const connection_db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: 'localhost',
